8c314d4f6c08df8b85bd8d0b2f114a9ff8ac6ca0
Panji <panji.bp.bp@gmail.com>
2023-05-06 15:29:31 +0700

    updates

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const canvas = document.getElementById("myCanvas");
-canvas.height = window.innerHeight;
 canvas.width = 200;
 
 const context = canvas.getContext("2d");
 const car = new Car(100,100,30,50);
-car.draw(context);
+
+animate();
+
+function animate() {
+    car.update();
+    
+    canvas.height = window.innerHeight;
+    car.draw(context);
+    requestAnimationFrame(animate);
+}
+
